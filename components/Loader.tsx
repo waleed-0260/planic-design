9979ea9551241af41d11dd821d46774ffcf36410
Waleed0260 <muhammadwaleedahsan43@gmail.com>
2025-01-25 02:35:11 -0800

    commit

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import loader from "../public/images/loader.gif"
 import Image from 'next/image'
+import Preloader from './PreLoader'
 
 const Loader = () => {
   return (
-    <div className='bg-black w-[100vw] h-full absolute flex items-center justify-center'>
-        <Image src={loader} alt="gjrit" className='object-cover'/>
+    <div className='bg-white w-[100vw] h-full absolute flex items-center justify-center'>
+        {/* <Image src={loader} alt="gjrit" className='object-cover'/> */}
+        <Preloader/>
     </div>
   )
 }
