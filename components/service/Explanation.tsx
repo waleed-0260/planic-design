d5b673669328e4defe60f54f7e5a4e87db0d0897
Waleed Ahsan <95635857+waleed-0260@users.noreply.github.com>
2025-08-12 22:13:04 +0500

    Update Explanation.tsx

--- components/service/Explanation.tsx
+++ components/service/Explanation.tsx
@@ -5,7 +5,7 @@ const Explanation = () => {
     <div className='flex flex-col w-[90%] my-[80px] space-y-9'>
         <h1 className='md:text-[52px] text-2xl leading-tight'>Services at Planic Design</h1>
         <p className='text-md textColor mt-2'>At Planic Design, we offer a comprehensive suite of interior design services tailored to transform spaces into thoughtful, livable environments. Whether you're renovating a home, designing a modern office, or planning a commercial project, our team blends creativity with precision to deliver solutions that reflect your style and goals. Below, you’ll find an overview of our core services, each designed to meet the unique demands of your space.</p>
-        <div className='grid grid-cols-2 md:grid-cols-4 md:space-x-5 space-y-5'>
+        <div className='md:grid hidden grid-cols-2 md:grid-cols-4 md:space-x-5 space-y-5'>
           <Link className='button' href={"#architecture"}>Architectural Design</Link>
           <Link className='button' href={"#renovation"}>Renovation & Fit Out</Link>
           <Link className='button' href={"#visulization"}>3D Visulization</Link>
